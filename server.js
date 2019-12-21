5a5c9d77da27acf73518c34e99969ee4fa76899f
Jeka89 <zoof24@gmail.com>
2019-12-21 19:59:24 +0200

    update server js

--- server.js
+++ server.js
@@ -1,34 +1,21 @@
-const {createServer} = require("http");
 const express = require("express");
-const compression = require("compression");
-const morgan = require("morgan");
 const path = require("path");
-
 const app = express();
-const dev = app.get('env') !== "production";
+const redirectToHTTPS = require("express-http-to-https").redirectToHTTPS;
+const compression = require("compression");
 
-const normalizePort = port => parseInt(port,10);
-const PORT = normalizePort(process.env.PORT || 5000);
+app.use(compression());
+// Don't redirect if the hostname is `localhost:port` or the route is `/insecure`
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
+app.use(express.static(path.join(__dirname, "build")));
 
-if(!dev){
-    app.disable("x-powered-by");
-    app.use(compression());
-    app.use(morgan("common"));
-    app.use(express.static(path.resolve(__dirname,'build')));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'build','index.html'));
-    })
-}
-
-if(dev){
-    app.use(morgan('dev'));
-}
-
-
-
-const server = createServer(app);
-
-server.listen(PORT,err=>{
-    if(err) throw err;
-    console.log("server work!");
+app.get("/", function(req, res) {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
+
+//handle all URLs
+app.get("*", function(req, res) {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+
+app.listen(process.env.PORT || 8080);
