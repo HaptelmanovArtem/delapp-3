6961bbc66cbfac2a8c7f7e36868fd72fd6f22a1c
Jeka89 <zoof24@gmail.com>
2019-12-21 20:13:39 +0200

    small fixes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+// import * as serviceWorker from './serviceWorker.js';
 
-import './index.css';
-import App from './App';
-import Store from './store.js';
+import "./index.css";
+import App from "./App";
+import Store from "./store.js";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Provider store={Store}>
-            <App />
-        </Provider>
-    </BrowserRouter>, 
-    document.getElementById('root'));
+  <BrowserRouter>
+    <Provider store={Store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
