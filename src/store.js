a10861628a37e6830abe37db9ea220598946370d
Jeka89 <zoof24@gmail.com>
2019-12-21 20:24:00 +0200

    add redux debugger

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
-import {createStore, applyMiddleware} from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import { createStore, compose, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
 
-import logger from 'redux-logger';
-import rootReducer from '../src/reducers/index.js';
+import logger from "redux-logger";
+import rootReducer from "../src/reducers/index.js";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
-const Store = createStore(rootReducer,applyMiddleware(logger,thunkMiddleware));
+const Store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger, thunkMiddleware))
+);
 
 export default Store;
